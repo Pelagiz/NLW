03b8c9641c564d99d87498dedc947d6b8db26d29
Pelagiz <wendel.paes.santos@gmail.com>
2021-11-02 20:43:15 -0300

    Update

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ const swiper = new Swiper(".swiper", {
     },
     mousewheel: true,
     keyboard: true,
+    breakpoints: { 762: { slidesPerView: 2, setWrapperSize: true } },
 });
 
 /* ScrollReveal: Mostrar elementos quando der Scroll na página */
@@ -63,16 +64,43 @@ footer .brand, footer .social
 
 /* Button back to top */
 
+const backToTopButton = document.querySelector(".back-to-top");
 const backToTop = () => {
-    const backToTopButton = document.querySelector(".back-to-top");
     if (window.scrollY >= 560) {
         backToTopButton.classList.add("show");
     } else {
         backToTopButton.classList.remove("show");
     }
 };
+/* Menu ativo conforme a seção visível na página  */
 
+const sections = document.querySelectorAll("main section[id]");
+const activateMenuAtCurrentSection = () => {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const sectionId = section.getAttribute("id");
+
+        const checkpointStart = checkpoint >= sectionTop;
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+        if (checkpointStart && checkpointEnd) {
+            document
+                .querySelector("nav ul li a[href*=" + sectionId + "]")
+                .classList.add("active");
+        } else {
+            document
+                .querySelector("nav ul li a[href*=" + sectionId + "]")
+                .classList.remove("active");
+        }
+    }
+};
+
+/* When Scroll */
 window.addEventListener("scroll", () => {
     changeHeaderWhenScroll();
     backToTop();
+    activateMenuAtCurrentSection();
 });
